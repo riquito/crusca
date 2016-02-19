ef294b7b3d8531c98ba60cf4c7cef9f74eda66a7
Riccardo Attilio Galli <riccardo@sideralis.org>
2016-02-19 23:09:09 +0100

    Parse commit messages using the loaded rules

--- src/github_bp.py
+++ src/github_bp.py
@@ -2,16 +2,28 @@
 from flask import Blueprint, jsonify
 # ours
 from .decorators import github_events
+from .rules import PickyReader
 
 bp = blueprint = Blueprint('github_bp', __name__)
+
+reader = PickyReader()
 
 
 @bp.route("/push-event", methods=['POST'])
 @github_events(['push'])
 def push_action():
+    errors = []
+    for commit in form['commits']:
+        try:
+            reader.analyze(commit['message'])
+        except UnacceptableContentError as err:
+            errors.push(commit['id'], err)
+
     return jsonify({})
 
 
 @bp.record_once
 def keep_config(state):
-    bp.config = state.app.config
+    config = state.app.config
+    for rule_name, config in config['RULES'].items():
+        reader.register(rule_name, config)
