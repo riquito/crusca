16e5d684f5c072e6441e8e33554b9e7d520d6951
Riccardo Attilio Galli <riccardo@sideralis.org>
2016-02-21 23:16:05 +0100

    Rule bad_start is case insensitive

--- src/rules/bad_start.py
+++ src/rules/bad_start.py
@@ -11,16 +11,18 @@ class BadStartingWordError(UnacceptableContentError):
 class BadStartRule(Rule):
 
     def __init__(self, config):
-        self.mustNot = config
+        self.bad_starts = config
 
-        if isinstance(self.mustNot, str):
-            self.mustNot = [self.mustNot]
+        if isinstance(self.bad_starts, str):
+            self.bad_starts = [self.bad_starts]
+
+        self.bad_starts = [x.lower() for x in self.bad_starts]
 
     def analyze(self, message):
-        words = message.split()
+        words = message.lower().split()
         word = words[0] if words else ''
 
-        if word in self.mustNot:
-            raise BadStartingWordError('Message cannot start with ' + word)
+        if word in self.bad_starts:
+            raise BadStartingWordError('Message cannot start with word \"{}\"'.format(word))
 
 ExportedRule = BadStartRule
--- tests/rules/test_bad_start.py
+++ tests/rules/test_bad_start.py
@@ -8,6 +8,14 @@ class BadStartTests(unittest.TestCase):
         c = ExportedRule(['bad1','bad2'])
         self.assertEqual(None, c.analyze('good'))
 
-    def test_start_with_forbidden_word(self):
+    def test_start_with_forbidden_word_provide_string(self):
+        c = ExportedRule('bad2')
+        self.assertRaises(UnacceptableContentError, c.analyze, 'bad2 is no good')
+
+    def test_start_with_forbidden_word_provide_list(self):
         c = ExportedRule(['bad1','bad2'])
         self.assertRaises(UnacceptableContentError, c.analyze, 'bad2 is no good')
+
+    def test_start_with_forbidden_word_different_case(self):
+        c = ExportedRule(['bad1','bad2'])
+        self.assertRaises(UnacceptableContentError, c.analyze, 'BaD2 is no good')
